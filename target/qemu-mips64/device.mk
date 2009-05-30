37175a9783c561c8a3f94b6818dabf512c3674e9
Waldemar Brodkorb <wbx@openadk.org>
2009-05-30 18:07:43 +0200

    qemu MIPS support
    - add support of all MIPS variants
    - MIPS32 boots fine with MALTA emulation
    - MIPS64 has a problem booting in 64 kernel mode,
      even linux 2.6.30rc7 halt on boot
    - remove Config.default

--- target/qemu-mips64/device.mk
+++ target/qemu-mips64/device.mk
@@ -1,7 +1,7 @@
 ARCH:=			mips
-CPU_ARCH:=		mips
+CPU_ARCH:=		mips64
 KERNEL_VERSION:=	2.6.29.1
 KERNEL_RELEASE:=	1
 KERNEL_MD5SUM:=		4ada43caecb08fe2af71b416b6f586d8
 TARGET_OPTIMIZATION:=	-Os -pipe
-TARGET_CFLAGS_ARCH:=    -march=mips32 -mabi=32
+TARGET_CFLAGS_ARCH:=    -march=mips64 -mabi=64
