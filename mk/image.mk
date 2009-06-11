89e8b4697cd4adfc6b49d19fe637560d65030302
Waldemar Brodkorb <wbx@openadk.org>
2009-06-11 18:33:11 +0200

    fix cpmac driver
    - at least dhcp and nfs mount works now
    - still problems with exception handling
    - userland does not work

--- mk/image.mk
+++ mk/image.mk
@@ -43,7 +43,7 @@ image-prepare-post:
 	chmod 1777 ${TARGET_DIR}/tmp
 
 INITRAMFS=	${DEVICE}-${ARCH}-${FS}
-ROOTFSSQUASHFS=	${DEVICE}-${ARCH}-${FS}.fimage
+ROOTFSSQUASHFS=	${DEVICE}-${ARCH}-${FS}.img
 ROOTFSTARBALL=	${DEVICE}-${ARCH}-${FS}.tar.gz
 
 ${BIN_DIR}/${ROOTFSTARBALL}: ${TARGET_DIR}
@@ -56,12 +56,10 @@ ${BIN_DIR}/${INITRAMFS}: ${TARGET_DIR}
 ${BIN_DIR}/${ROOTFSSQUASHFS}: ${TARGET_DIR}
 	PATH='${TARGET_PATH}' \
 	mksquashfs ${TARGET_DIR} ${BUILD_DIR}/root.squashfs \
-		-nopad -noappend -root-owned -le $(MAKE_TRACE)
+		-nopad -noappend -root-owned $(MAKE_TRACE)
 	# padding of images is required
 	cat ${BIN_DIR}/${DEVICE}-${ARCH}-kernel ${BUILD_DIR}/root.squashfs > \
 		${BUILD_DIR}/${ROOTFSSQUASHFS}
-	dd if=${BUILD_DIR}/${ROOTFSSQUASHFS} of=${BIN_DIR}/${ROOTFSSQUASHFS} \
-		bs=4063232 conv=sync $(MAKE_TRACE)
 
 imageclean:
 	rm -f $(BIN_DIR)/$(DEVICE)-*
