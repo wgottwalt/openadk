e6d7923746537504a5e19b5c9aa050a1836048b6
Waldemar Brodkorb <wbx@openadk.org>
2010-01-03 14:46:58 +0100

    move package template out of pkgmaker's view
    avoid pkgmaker warning. quagga is a multi package.

--- mk/build.mk
+++ mk/build.mk
@@ -178,10 +178,10 @@ kernelconfig:
 	$(MAKE) -C $(BUILD_DIR)/linux/ ARCH=$(ARCH) menuconfig
 	cp $(BUILD_DIR)/linux/.config $(TOPDIR)/target/$(ADK_TARGET)/kernel.config
 
-# create a new package from package/template
+# create a new package from package/.template
 newpackage:
 	@echo "Creating new package $(PKG)"
-	$(CP) $(TOPDIR)/package/template $(TOPDIR)/package/$(PKG)
+	$(CP) $(TOPDIR)/package/.template $(TOPDIR)/package/$(PKG)
 	pkg=$$(echo $(PKG)|tr '[:lower:]' '[:upper:]'); \
 		$(SED) "s#@UPKG@#$$pkg#" $(TOPDIR)/package/$(PKG)/Makefile
 	$(SED) 's#@PKG@#$(PKG)#' $(TOPDIR)/package/$(PKG)/Makefile
@@ -403,17 +403,17 @@ bulk:
 		mkdir -p $(TOPDIR)/bulkdir/$$target-$$libc-$$fs; \
 	    ( \
 		echo === building $$target $$libc $$fs on $$(date); \
-		$(MAKE) prereq && \
+		$(GMAKE) prereq && \
 		if [ "x$$p" = xy ];then \
-			$(MAKE) TARGET=$$target LIBC=$$libc FS=$$fs PKG=ipkg \
+			$(GMAKE) TARGET=$$target LIBC=$$libc FS=$$fs PKG=ipkg \
 				allmodconfig; \
 		else \
-			$(MAKE) TARGET=$$target LIBC=$$libc FS=$$fs PKG=ipkg \
+			$(GMAKE) TARGET=$$target LIBC=$$libc FS=$$fs PKG=ipkg \
 				defconfig; \
 		fi && \
-		$(MAKE) VERBOSE=1 -f mk/build.mk allcopy \
+		$(GMAKE) VERBOSE=1 -f mk/build.mk allcopy \
 		    targetdir=$$target-$$libc-$$fs; \
-		$(MAKE) cleantarget; \
+		$(GMAKE) cleantarget; \
 		rm .config; \
 	    ) 2>&1 | tee $(TOPDIR)/bulkdir/$$target-$$libc-$$fs/log; \
 	done <${TOPDIR}/target/bulk.lst
