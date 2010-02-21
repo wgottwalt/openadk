b8074629774161e2cea19c704aaea9d3e482e290
Waldemar Brodkorb <wbx@openadk.org>
2010-02-21 18:51:31 +0100

    add some finetuning stuff for foxg20 support

--- target/foxg20/target.mk
+++ target/foxg20/target.mk
@@ -1,5 +1,6 @@
+# arm default is little endian, this target uses EABI
 ARCH:=			arm
-CPU_ARCH:=		armel
+CPU_ARCH:=		arm
 KERNEL_VERSION:=	2.6.32.2
 KERNEL_RELEASE:=	1
 KERNEL_MD5SUM:=		260551284ac224c3a43c4adac7df4879
