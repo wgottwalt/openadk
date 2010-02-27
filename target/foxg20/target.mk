a4a69676b48a78e175e9f3d6b03df8b5ab29c5c1
Waldemar Brodkorb <wbx@openadk.org>
2010-02-27 22:15:34 +0100

    update foxg20 to 2.6.33, fix rsync reconfigure bug

--- target/foxg20/target.mk
+++ target/foxg20/target.mk
@@ -1,8 +1,8 @@
 # arm default is little endian, this target uses EABI
 ARCH:=			arm
 CPU_ARCH:=		arm
-KERNEL_VERSION:=	2.6.32.2
+KERNEL_VERSION:=	2.6.33
 KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		260551284ac224c3a43c4adac7df4879
+KERNEL_MD5SUM:=		c3883760b18d50e8d78819c54d579b00
 TARGET_OPTIMIZATION:=	-Os -pipe
 TARGET_CFLAGS_ARCH:=    -march=armv5te -mtune=arm926ej-s
