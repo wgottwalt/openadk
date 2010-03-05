8d99513b10d0b93c1468a8f39218d58f2e861d7f
Waldemar Brodkorb <wbx@openadk.org>
2010-03-05 18:39:21 +0100

    update to 2.6.33

--- target/native/target.mk
+++ target/native/target.mk
@@ -1,6 +1,6 @@
 ARCH:=			$(shell uname -m|sed -e "s/i.*86/x86/" -e "s/_64//")
 CPU_ARCH:=		$(shell uname -m)
-KERNEL_VERSION:=	2.6.32
+KERNEL_VERSION:=	2.6.33
 KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		260551284ac224c3a43c4adac7df4879
+KERNEL_MD5SUM:=		c3883760b18d50e8d78819c54d579b00
 TARGET_OPTIMIZATION:=	-Os -pipe
