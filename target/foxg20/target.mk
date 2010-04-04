78127212d0aaa988eb05226879b59c6238a287b3
Waldemar Brodkorb <wbx@openadk.org>
2010-04-04 13:49:19 +0200

    update kernel and uclibc config

--- target/foxg20/target.mk
+++ target/foxg20/target.mk
@@ -1,8 +1,8 @@
 # arm default is little endian, this target uses EABI
 ARCH:=			arm
 CPU_ARCH:=		arm
-KERNEL_VERSION:=	2.6.33
+KERNEL_VERSION:=	2.6.33.2
 KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		c3883760b18d50e8d78819c54d579b00
+KERNEL_MD5SUM:=		80c5ff544b0ee4d9b5d8b8b89d4a0ef9
 TARGET_OPTIMIZATION:=	-Os -pipe
 TARGET_CFLAGS_ARCH:=    -march=armv5te -mtune=arm926ej-s
