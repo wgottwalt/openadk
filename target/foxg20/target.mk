92dd4fee15cb7c33868c7d7213f82136955397b1
Waldemar Brodkorb <wbx@openadk.org>
2010-05-29 14:29:22 +0200

    upgrade all targets to Linux 2.6.34

--- target/foxg20/target.mk
+++ target/foxg20/target.mk
@@ -1,8 +1,8 @@
 # arm default is little endian, this target uses EABI
 ARCH:=			arm
 CPU_ARCH:=		arm
-KERNEL_VERSION:=	2.6.33.2
+KERNEL_VERSION:=	2.6.34
 KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		80c5ff544b0ee4d9b5d8b8b89d4a0ef9
+KERNEL_MD5SUM:=		10eebcb0178fb4540e2165bfd7efc7ad
 TARGET_OPTIMIZATION:=	-Os -pipe
 TARGET_CFLAGS_ARCH:=    -march=armv5te -mtune=arm926ej-s
