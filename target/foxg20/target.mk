a5841ff67451430f6ecd4d4b33d30fcf90e77121
Waldemar Brodkorb <wbx@openadk.org>
2010-09-18 20:01:00 +0200

    update foxg20 kernel and config

--- target/foxg20/target.mk
+++ target/foxg20/target.mk
@@ -1,8 +1,8 @@
 # arm default is little endian, this target uses EABI
 ARCH:=			arm
 CPU_ARCH:=		arm
-KERNEL_VERSION:=	2.6.34
+KERNEL_VERSION:=	2.6.35.4
 KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		10eebcb0178fb4540e2165bfd7efc7ad
+KERNEL_MD5SUM:=		0bb2cd59c13d7412f813c8fbc0769eec
 TARGET_OPTIMIZATION:=	-Os -pipe
 TARGET_CFLAGS_ARCH:=    -march=armv5te -mtune=arm926ej-s
