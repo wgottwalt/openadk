664dea7d95839a01be7c551add09994bce7be959
Waldemar Brodkorb <wbx@openadk.org>
2010-11-17 19:08:43 +0100

    update all targets to 2.6.36

--- target/toolchain-arm/target.mk
+++ target/toolchain-arm/target.mk
@@ -1,7 +1,5 @@
+include $(TOPDIR)/mk/kernel-ver.mk
 ARCH:=			arm
 CPU_ARCH:=		arm
-KERNEL_VERSION:=	2.6.35.7
-KERNEL_RELEASE:=	1
-KERNEL_MD5SUM:=		f741879bcd3a5366a1bbe0ad5cdb7935
 TARGET_OPTIMIZATION:=	-Os -pipe
 TARGET_CFLAGS_ARCH:=    -msoft-float
