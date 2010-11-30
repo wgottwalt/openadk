5265f6b35db3d3cd1af9cb64936ada0e62ed9f32
Waldemar Brodkorb <wbx@openadk.org>
2010-11-30 21:20:55 +0100

    make ACPI choosable and enable for ibm x40

---
+++ target/linux/config/Config.in.pm
@@ -0,0 +1,43 @@
+config ADK_KERNEL_PM
+	boolean
+
+config ADK_KERNEL_ACPI
+	boolean
+
+config ADK_KERNEL_ACPI_SYSFS_POWER
+	boolean
+
+config ADK_KERNEL_ACPI_AC
+	boolean
+
+config ADK_KERNEL_ACPI_BATTERY
+	boolean
+
+config ADK_KERNEL_ACPI_BUTTON
+	boolean
+
+config ADK_KERNEL_ACPI_FAN
+	boolean
+
+config ADK_KERNEL_ACPI_DOCK
+	boolean
+
+menu "Power Management support"
+
+config ADK_HARDWARE_ACPI
+	prompt "Enable ACPI support"
+	boolean
+	select ADK_KERNEL_PM
+	select ADK_KERNEL_ACPI
+	select ADK_KERNEL_ACPI_SYSFS_POWER
+	select ADK_KERNEL_ACPI_AC
+	select ADK_KERNEL_ACPI_BATTERY
+	select ADK_KERNEL_ACPI_BUTTON
+	select ADK_KERNEL_ACPI_FAN
+	select ADK_KERNEL_ACPI_DOCK
+	default y if ADK_HARDWARE_IBM_X40
+	default n
+	help
+	 Enable ACPI support.
+
+endmenu
