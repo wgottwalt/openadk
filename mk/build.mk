1edd657512fe89c6727a3d8adb68f29568078347
Waldemar Brodkorb <wbx@openadk.org>
2011-01-10 12:17:41 +0100

    create directory where others are created

--- mk/build.mk
+++ mk/build.mk
@@ -136,7 +136,7 @@ ifeq ($(ADK_TARGET_PACKAGE_IPKG),y)
 endif
 
 ${STAGING_TARGET_DIR} ${STAGING_TARGET_DIR}/etc ${STAGING_HOST_DIR}:
-	mkdir -p ${STAGING_TARGET_DIR}/{bin,etc,lib,usr/include} \
+	mkdir -p ${STAGING_TARGET_DIR}/{bin,etc,lib,usr/include,usr/lib} \
 		${STAGING_HOST_DIR}/{bin,lib}
 
 ${STAGING_TARGET_DIR}/etc/ipkg.conf: ${STAGING_TARGET_DIR}/etc
