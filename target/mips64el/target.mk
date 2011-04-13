b5a0fff870a44d83222e71331cac269f021652a3
wbx <wbx@openadk.org>
2011-04-13 09:52:52 +0200

    use the right TARGET_CFLAGS

--- target/mips64el/target.mk
+++ target/mips64el/target.mk
@@ -2,4 +2,4 @@ include $(TOPDIR)/mk/kernel-ver.mk
 ARCH:=			mips
 CPU_ARCH:=		mips64el
 TARGET_OPTIMIZATION:=	-Os -pipe
-TARGET_CFLAGS_ARCH:=    -march=mips64 -mabi=64
+TARGET_CFLAGS_ARCH:=    $(ADK_TARGET_CFLAGS) -mabi=64
