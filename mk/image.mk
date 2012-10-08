b4e40d3ec069054c9e1ef0b2d9ac026676073267
Waldemar Brodkorb <mail@waldemar-brodkorb.de>
2012-10-09 00:05:57 +0200

    handle /usr/lib on target as symlink to default library

--- mk/image.mk
+++ mk/image.mk
@@ -51,9 +51,13 @@ image-prepare-post:
 	-rm -f ${TARGET_DIR}/bin/sh
 	ln -sf ${BINSH} ${TARGET_DIR}/bin/sh
 ifeq ($(ADK_TARGET_WITH_MULTILIB),y)
+	# multilib fixup lib dirs
 	mv ${TARGET_DIR}/lib/* ${TARGET_DIR}/${ADK_TARGET_LIBC_PATH}
 	rm -rf ${TARGET_DIR}/lib/
 	ln -sf /${ADK_TARGET_LIBC_PATH} ${TARGET_DIR}/lib
+	mv ${TARGET_DIR}/usr/lib/* ${TARGET_DIR}/usr/${ADK_TARGET_LIBC_PATH}
+	rm -rf ${TARGET_DIR}/usr/lib/
+	ln -sf /usr/${ADK_TARGET_LIBC_PATH} ${TARGET_DIR}/usr/lib
 endif
 
 KERNEL_PKGDIR:=$(LINUX_BUILD_DIR)/kernel-pkg
