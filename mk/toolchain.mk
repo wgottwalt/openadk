581c747ae157d8802dc704786eaceb648da79e28
Waldemar Brodkorb <mail@waldemar-brodkorb.de>
2012-10-10 10:54:20 +0200

    fix 32 Bit multilib builds

--- mk/toolchain.mk
+++ mk/toolchain.mk
@@ -2,5 +2,6 @@ prepare: ${WRKDIST}/.prepared $(WRKBUILD)/.headers
 configure: ${WRKBUILD}/.configured
 compile: $(WRKBUILD)/.compiled
 install: $(WRKBUILD)/.installed
+fixup: $(WRKBUILD)/.fixup
 clean:
 	rm -rf $(WRKDIR)
