62b730710ddbc13d7137b453199a4eb23c3e95f6
Waldemar Brodkorb <wbx@openadk.org>
2013-09-15 14:15:43 +0200

    fixup musl-gcc patch, reported upstream

--- target/ppc/target.mk
+++ target/ppc/target.mk
@@ -2,4 +2,4 @@ include $(TOPDIR)/mk/kernel-ver.mk
 ARCH:=			powerpc
 CPU_ARCH:=		ppc
 TARGET_OPTIMIZATION:=	-Os -pipe
-TARGET_CFLAGS_ARCH:=	$(ADK_TARGET_CFLAGS)
+TARGET_CFLAGS_ARCH:=	$(ADK_TARGET_CFLAGS) -Wl,--secure-plt
