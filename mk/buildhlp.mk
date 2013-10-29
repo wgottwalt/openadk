21795cc1a20760ff37bca88b799fef664e70ccf2
Waldemar Brodkorb <wbx@openadk.org>
2013-10-29 10:32:23 +0100

    convert tslib to autotool infrastructure, downgrade m4, newest version have problems generating correct Makefiles

--- mk/buildhlp.mk
+++ mk/buildhlp.mk
@@ -95,7 +95,7 @@ else
 endif
 	@-test -r ${WRKDIR}/.autoreconf_done && \
 		(cd ${WRKDIR}.orig/${PKG_NAME}-${PKG_VERSION}; \
-		env ${AUTOTOOL_ENV} autoreconf -if)
+		env ${AUTOTOOL_ENV} autoreconf -if) $(MAKE_TRACE)
 	@rm -rf ${WRKDIR}.orig/${PKG_NAME}-${PKG_VERSION}/autom4te.cache
 	@# restore config.sub/config.guess
 	@for i in $$(find ${WRKDIR}.orig -name config.sub);do \
