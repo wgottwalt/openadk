5ce633839096d25a684af6978ba9da7d6d34fdaf
Waldemar Brodkorb <wbx@openadk.org>
2013-10-30 15:46:44 +0100

    fix update-patches when WRKDIST is overwritten in the Makefile

--- mk/buildhlp.mk
+++ mk/buildhlp.mk
@@ -94,7 +94,8 @@ else
 	@$(MAKE) -s V=0 prepare WRKDIR=${WRKDIR}.orig PREVENT_PATCH=: NO_CHECKSUM=1
 endif
 	@-test ! -r ${WRKDIR}/.autoreconf_done || \
-		(cd ${WRKDIR}.orig/${PKG_NAME}-${PKG_VERSION}; \
+		(wrkdist=$(WRKDIST) dir=$${wrkdist#$(WRKDIR)}; \
+		cd ${WRKDIR}.orig$${dir}; \
 		env ${AUTOTOOL_ENV} autoreconf -if) $(MAKE_TRACE)
 	@rm -rf ${WRKDIR}.orig/${PKG_NAME}-${PKG_VERSION}/autom4te.cache
 	@# restore config.sub/config.guess
