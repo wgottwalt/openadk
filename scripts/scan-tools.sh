77dd9c466922fad94a8514f071a4bc87fc2ed0a9
Waldemar Brodkorb <wbx@openadk.org>
2014-03-31 08:34:05 +0200

    use gnu find on darwin

--- scripts/scan-tools.sh
+++ scripts/scan-tools.sh
@@ -247,16 +247,20 @@ fi
 
 host_build_findutils=0
 if ! which gxargs >/dev/null 2>&1; then
-	if ! which xargs >/dev/null 2>&1; then
-		echo "No xargs found, will build one."
-		host_build_findutils=1
+	if which xargs >/dev/null 2>&1; then
+		if ! xargs --version 2>/dev/null|grep GNU >/dev/null;then
+			echo "No GNU xargs found, will build one."
+			host_build_findutils=1
+		fi
 	fi
 fi
 
 if ! which gfind >/dev/null 2>&1; then
-	if ! which find >/dev/null 2>&1; then
-		echo "No find found, will build one."
-		host_build_findutils=1
+	if which find >/dev/null 2>&1; then
+		if ! find --version 2>/dev/null|grep GNU >/dev/null;then
+			echo "No GNU find found, will build one."
+			host_build_findutils=1
+		fi
 	fi
 fi
 
