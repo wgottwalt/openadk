5c5b24f4139bbce45beb31473247d550520035d1
Waldemar Brodkorb <wbx@openadk.org>
2015-05-01 21:43:44 +0200

    use HOST_CFLAGS for Kconfig and adk tools

--- scripts/scan-tools.sh
+++ scripts/scan-tools.sh
@@ -100,6 +100,7 @@ X=$($makecmd ADK_TOPDIR=$topdir LDADD=-lgcc 2>&1)
 if [[ $X != *@(Native compiler works)* ]]; then
 	echo Cannot compile with shared libgcc use static one.
 	HOST_CFLAGS=-static-libgcc
+	echo "HOST_CFLAGS:=-static-libgcc" >> $topdir/prereq.mk
 fi
 
 X=$($makecmd ADK_TOPDIR=$topdir HOST_CFLAGS=${HOST_CFLAGS} 2>&1)
