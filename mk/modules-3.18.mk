a8428de05cd6ad86dba8444de78a64294d4ddb5e
Waldemar Brodkorb <wbx@openadk.org>
2015-05-02 15:24:16 -0500

    update kernel versions, add separate modules-<version>.mk files to abstract patch name changes

---
+++ mk/modules-3.18.mk
@@ -0,0 +1,5 @@
+# This file is part of the OpenADK project. OpenADK is copyrighted
+# material, please see the LICENCE file in the top-level directory.
+
+USBMODULES:=drivers/usb/common/usb-common drivers/usb/core/usbcore
+USBUDC:=gadget/udc
