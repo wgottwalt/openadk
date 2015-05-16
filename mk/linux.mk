b6cdac7360b0a1abe467ee91095726804b602fbd
Waldemar Brodkorb <wbx@openadk.org>
2015-05-16 18:40:37 +0200

    update to latest 4.x kernel

--- mk/linux.mk
+++ mk/linux.mk
@@ -5,7 +5,8 @@ PKG_NAME:=	linux
 PKG_VERSION:=	$(KERNEL_VERSION)
 PKG_RELEASE:=	$(KERNEL_RELEASE)
 PKG_HASH:=	$(KERNEL_HASH)
-PKG_SITES:=  	${MASTER_SITE_KERNEL:=kernel/v3.0/} \
+PKG_SITES:=  	${MASTER_SITE_KERNEL:=kernel/v4.x/} \
+		${MASTER_SITE_KERNEL:=kernel/v3.x/} \
 		${MASTER_SITE_KERNEL:=kernel/v3.0/testing/} \
 		${MASTER_SITE_KERNEL:=kernel/v2.6/longterm/v2.6.32/}
 DISTFILES=	$(PKG_NAME)-$(PKG_VERSION).tar.xz
