aaf60c90f5a15e356e66d5929ef73988a326e30c
Waldemar Brodkorb <wbx@uclibc-ng.org>
2015-10-30 10:32:21 +0100

    default to static device nodes for noMMU systems, add basic device nodes to initramfs

--- mk/image.mk
+++ mk/image.mk
@@ -133,6 +133,18 @@ ${FW_DIR}/${ROOTFSUSERTARBALL}: ${TARGET_DIR}/.adk
 ${STAGING_TARGET_DIR}/${INITRAMFS}_list: ${TARGET_DIR}/.adk
 	env PATH='${HOST_PATH}' $(BASH) ${LINUX_DIR}/scripts/gen_initramfs_list.sh -u squash -g squash \
 		${TARGET_DIR}/ >$@
+	( \
+		echo "nod /dev/console 0644 0 0 c 5 1"; \
+		echo "nod /dev/tty 0644 0 0 c 5 0"; \
+		for i in 0 1 2 3 4; do \
+			echo "nod /dev/tty$$i 0644 0 0 c 4 $$$$i"; \
+		done; \
+		echo "nod /dev/null 0644 0 0 c 1 3"; \
+		echo "nod /dev/ram 0655 0 0 b 1 1"; \
+		echo "nod /dev/ttyS0 0660 0 0 c 4 64"; \
+		echo "nod /dev/ttyS1 0660 0 0 c 4 65"; \
+	) >>$@
+
 
 ${FW_DIR}/${INITRAMFS}: ${STAGING_TARGET_DIR}/${INITRAMFS}_list
 	${LINUX_DIR}/usr/gen_init_cpio ${STAGING_TARGET_DIR}/${INITRAMFS}_list | \
