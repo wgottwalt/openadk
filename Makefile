wgottwalt/openadk

commit a94d74d9c875b4a1c07945bf9af282221d0178b3

rework prereq check
The new prereq check is completely implemented in
POSIX shell in scripts/prereq.sh.

It combines the old features from Makefile, scan-tools.sh,
scan-pkgs.sh, reloc.sh and some wrappers for tools.

The big benefit is to have all portability stuff in one place.
Furthermore we can compile GNU make and bash on the fly, for
systems lacking the required tools.
All changes on the host are detected on the fly, no make
prereq required anymore.

The build process is separated in following three phases:
1. small wrapper Makefile is used for BSD make or GNU make
2. prereq.sh is called, doing all checking, calling Makefile.adk
3. old logic in Makefile.adk or mk/build.mk is used

Tested successfully on Linux, MacOS X, Cygwin, FreeBSD, OpenBSD
and NetBSD.

An old depmaker bug was fixed, only optional host tools are compiled.
For example, even when a host provides xz, a local xz was compiled
in the past, because other packages had a build dependency on it.

Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>

Makefile
@@ -1,221 +1,8 @@
 # This file is part of the OpenADK project. OpenADK is copyrighted
 # material, please see the LICENCE file in the top-level directory.
 
-_UNLIMIT=	__limit=$$(ulimit -dH 2>/dev/null); \
-		test -n "$$__limit" && ulimit -Sd $$__limit; ulimit -n 1024;
+# GNU make and BSD make compatible make file wrapper
+MAKECMDGOALS+= ${.TARGETS}
 
-all: .prereq_done checkreloc
-	@${_UNLIMIT} ${GMAKE_INV} all
-
-v: .prereq_done
-	@(echo; echo "Build started on $$(LC_ALL=C LANGUAGE=C date)"; \
-	    set -x; ${_UNLIMIT} ${GMAKE_FMK} ADK_VERBOSE=1 all) 2>&1 | tee -a make.log
-
-help:
-	@echo 'Configuration targets:'
-	@echo '  config       - Update current config utilising a line-oriented program'
-	@echo '  menuconfig   - Update current config utilising a menu based program'
-	@echo '                 (default when .config does not exist)'
-	@echo '  oldconfig    - Update current config utilising a provided .configs base'
-	@echo '  defconfig    - New config with defaults'
-	@echo '  allmodconfig - New config selecting all symbols with m'
-	@echo '  allyesconfig - New config selecting all symbols with y'
-	@echo '  allnoconfig  - New config where all options are answered with no'
-	@echo ''
-	@echo 'Help targets:'
-	@echo '  help         - Print this help text'
-	@echo '  pkg-help     - Print help about selectively compiling single packages'
-	@echo '  dev-help     - Print help for developers / package maintainers'
-	@echo ''
-	@echo 'Common targets:'
-	@echo '  download     - fetches all needed distfiles'
-	@echo '  kernelconfig - view the target kernel configuration'
-	@echo ''
-	@echo 'Cleaning targets:'
-	@echo '  clean        - Remove firmware and build directories'
-	@echo '  cleandir     - Same as "clean", but also remove all built toolchains'
-	@echo '  cleansystem  - Same as "cleandir", but only remove active system'
-	@echo '  cleankernel  - Remove kernel dir, useful if you changed any kernel patches'
-	@echo '  distclean    - Same as "cleandir", but also remove downloaded'
-	@echo '                 distfiles and .config'
-	@echo ''
-	@echo 'Other generic targets:'
-	@echo '  all          - Build everything as specified in .config'
-	@echo '                 (default if .config exists)'
-	@echo '  v            - Same as "all" but with logging to make.log enabled'
-
-pkg-help:
-	@echo 'Package specific targets (use with "package=<pkg-name>" parameter):'
-	@echo '  fetch        - Download the necessary distfile'
-	@echo '  extract      - Same as "fetch", but also extract the distfile'
-	@echo '  patch        - Same as "extract", but also patch the source'
-	@echo '  build        - Same as "patch", but also build the binaries'
-	@echo '  fake         - Same as "build", but also install the binaries'
-	@echo '  package      - Same as "fake", but also create the package'
-	@echo '  clean        - Deinstall and remove the build area'
-	@echo '  distclean    - Same as "clean", but also remove the distfiles'
-	@echo ''
-	@echo 'Short package rebuilding guide:'
-	@echo '  run "make package=<pkgname> clean" to remove all generated binaries'
-	@echo '  run "make package=<pkgname> package" to build everything and create the package(s)'
-	@echo ''
-	@echo 'This does not automatically resolve package dependencies!'
-
-dev-help:
-	@echo 'Fast way of updating package patches:'
-	@echo '  run "make package=<pkgname> clean" to start with a good base'
-	@echo '  run "make package=<pkgname> patch" to fetch, unpack and patch the source'
-	@echo '  edit the package sources at build_dir/w-<pkgname>-*/<pkgname>-<version>'
-	@echo '  run "make package=<pkgname> update-patches" to regenerate patch files'
-	@echo ''
-	@echo 'All changed patches will be opened with your $$EDITOR,'
-	@echo 'so you can add a description and verify the modifications.'
-	@echo ''
-	@echo 'Adding a new package:'
-	@echo 'make PKG=foo VER=1.0 newpackage'
-	@echo 'Adding a new simple library package:'
-	@echo 'make PKG=foo VER=1.0 TYPE=lib newpackage'
-	@echo 'Adding a new simple program package:'
-	@echo 'make PKG=foo VER=1.0 TYPE=prog newpackage'
-
-clean: .prereq_done
-	-@rm -f nohup.out
-	@${GMAKE_INV} clean
-
-config: .prereq_done
-	@${GMAKE_INV} _config W=
-
-oldconfig: .prereq_done
-	@${GMAKE_INV} _config W=--oldconfig
-
-download: .prereq_done
-	@${GMAKE_INV} toolchain/download
-	@${GMAKE_INV} dep
-	@${GMAKE_INV} package/download
-
-cleankernel kernelclean: .prereq_done
-	-@${GMAKE_INV} cleankernel
-
-cleandir dirclean: .prereq_done
-	-@${GMAKE_INV} cleandir
-	@-rm -f make.log .prereq_done
-
-cleansystem: .prereq_done
-	-@${GMAKE_INV} cleansystem
-	@-rm -f make.log .prereq_done
-
-distclean cleandist:
-	-@${GMAKE_INV} distclean
-	@-rm -f make.log .prereq_done
-
-image: .prereq_done
-	@${GMAKE_INV} image
-
-targethelp: .prereq_done
-	@${GMAKE_INV} targethelp
-
-kernelconfig: .prereq_done
-	@${GMAKE_INV} kernelconfig
-
-newpackage: .prereq_done
-	@${GMAKE_INV} newpackage
-
-image_clean imageclean cleanimage: .prereq_done
-	@${GMAKE_INV} image_clean
-
-menuconfig: .prereq_done
-	@${GMAKE_INV} menuconfig
-
-defconfig: .prereq_done
-	@${GMAKE_INV} defconfig
-
-allnoconfig: .prereq_done
-	@${GMAKE_INV} KCONFIG_ALLCONFIG=all.config _config W=--allnoconfig
-
-allyesconfig: .prereq_done
-	@${GMAKE_INV} KCONFIG_ALLCONFIG=all.config _config W=--allyesconfig
-
-allmodconfig: .prereq_done
-	@${GMAKE_INV} KCONFIG_ALLCONFIG=all.config _config W=--allmodconfig
-
-package_index: .prereq_done
-	@${GMAKE_INV} package_index
-
-buildall: .prereq_done
-	@${GMAKE_INV} buildall
-
-check: .prereq_done
-	@${GMAKE_INV} check
-
-check-gcc: .prereq_done
-	@${GMAKE_INV} check-gcc
-
-check-g++: .prereq_done
-	@${GMAKE_INV} check-g++
-
-menu: .prereq_done
-	@${GMAKE_INV} menu
-
-dep: .prereq_done
-	@${GMAKE_INV} dep
-
-world: .prereq_done
-	@${GMAKE_INV} world
-
-prereq:
-	@rm -f .prereq_done
-	@${GMAKE} .prereq_done
-
-prereq-noerror:
-	@rm -f .prereq_done
-	@${GMAKE} .prereq_done NO_ERROR=1
-
-NO_ERROR=0
-.prereq_done:
-	@-rm -rf .prereq_done
-	@if ! bash --version 2>&1 | grep -F 'GNU bash' >/dev/null 2>&1; then \
-		echo "GNU bash needs to be installed."; \
-		exit 1; \
-	fi
-	@echo "ADK_TOPDIR:=$$(readlink -nf . 2>/dev/null || pwd -P)" >prereq.mk
-	@echo "BASH:=$$(which bash)" >>prereq.mk
-	@if [ -z "$$(which gmake 2>/dev/null )" ]; then \
-		echo "GMAKE:=$$(which make)" >>prereq.mk ;\
-	else \
-		echo "GMAKE:=$$(which gmake)" >>prereq.mk ;\
-	fi
-	@echo "GNU_HOST_NAME:=$$(${CC} -dumpmachine)" >>prereq.mk
-	@echo "ARCH_FOR_BUILD:=$$(${CC} -dumpmachine | sed \
-	    -e 's/x86_64-linux-gnux32/x32/' \
-	    -e s'/-.*//' \
-	    -e 's/sparc.*/sparc/' \
-	    -e 's/armeb.*/armeb/g' \
-	    -e 's/arm.*/arm/g' \
-	    -e 's/m68k.*/m68k/' \
-	    -e 's/sh[234]/sh/' \
-	    -e 's/mips-.*/mips/' \
-	    -e 's/mipsel-.*/mipsel/' \
-	    -e 's/i[3-9]86/x86/' \
-	    )" >>prereq.mk
-	@echo 'HOST_CC:=${CC}' >>prereq.mk
-	@echo 'HOST_CXX:=${CXX}' >>prereq.mk
-	@echo 'LANGUAGE:=C' >>prereq.mk
-	@echo 'LC_ALL:=C' >>prereq.mk
-	@echo 'MAKE:=$${GMAKE}' >>prereq.mk
-	@echo "OStype:=$$(env uname)" >>prereq.mk
-	@echo "_PATH:=$$PATH" >>prereq.mk
-	@echo "PATH:=\$${ADK_TOPDIR}/scripts:/usr/sbin:$$PATH" >>prereq.mk
-	@echo "SHELL:=$$(which bash)" >>prereq.mk
-	@echo "GIT:=$$(which git 2>/dev/null)" >>prereq.mk
-	@env NO_ERROR=${NO_ERROR} BASH="$$(which bash)" \
-		CC='${CC}' CPPFLAGS='${CPPFLAGS}' \
-	    	bash scripts/scan-tools.sh
-	@echo '===> Prerequisites checked successfully.'
-	@touch .adkinit
-	@touch $@
-
-checkreloc:
-	@bash scripts/reloc.sh
-
-.PHONY: prereq prereq-noerror checkreloc
-# DO NOT DELETE
+all v help targethelp kernelconfig image menuconfig download clean cleankernel cleansystem cleandir distclean hostclean hostpackage fetch package extract patch dep menu:
+	@./scripts/prereq.sh ${MAKECMDGOALS}

adk/config/Makefile
@@ -69,19 +69,19 @@ MCONF_OBJS=$(patsubst %.c,%.o, $(MCONF_SRC))
 SHARED_OBJS=$(patsubst %.c,%.o, $(SHARED_SRC))
 
 conf: $(CONF_OBJS) $(SHARED_OBJS)
-	@$(HOST_CC) $(HOST_CFLAGS) $^ -o $@
+	@$(HOST_CC) $(HOST_CFLAGS) $^ -o $@ 2>/dev/null
 
 mconf: $(MCONF_OBJS) $(SHARED_OBJS)
-	@$(HOST_CC) $(HOST_CFLAGS) $^ -o $@ $(LIBS)
+	@$(HOST_CC) $(HOST_CFLAGS) $^ -o $@ $(LIBS) 2>/dev/null
 
 $(CONF_OBJS): %.o : %.c $(SHARED_DEPS)
-	@$(HOST_CC) $(HOST_CFLAGS) -I. -c $< -o $@
+	@$(HOST_CC) $(HOST_CFLAGS) -I. -c $< -o $@ 2>/dev/null
 
 $(MCONF_OBJS): %.o : %.c $(SHARED_DEPS)
-	@$(HOST_CC) $(HOST_CFLAGS) -I. -c $< -o $@
+	@$(HOST_CC) $(HOST_CFLAGS) -I. -c $< -o $@ 2>/dev/null
 
 glob.o: glob.c $(SHARED_DEPS)
-	@$(HOST_CC) $(HOST_CFLAGS) -I. -c glob.c -o $@
+	@$(HOST_CC) $(HOST_CFLAGS) -I. -c glob.c -o $@ 2>/dev/null
 
 lkc_defs.h: lkc_proto.h
 	@sed < $< > $@ 's/P(\([^,]*\),.*/#define \1 (\*\1_p)/'

package/Makefile
@@ -50,19 +50,19 @@ install: base-files-install $(INSTALL_ICONV) $(INSTALL_PACKAGES)
 endif
 
 %-download:
-	$(START_TRACE) "package/$(patsubst %-download,%,$@)-download: "
+	$(START_TRACE) "package/$(patsubst %-download,%,$@)-download.. "
 	$(MAKE) -C $(patsubst %-download,%,$@) fetch
 	$(CMD_TRACE) " done"
 	$(END_TRACE)
 
 %-host-compile:
-	$(START_TRACE) "package/$(patsubst %-host-compile,%,$@)-host-compile: "
+	$(START_TRACE) "package/$(patsubst %-host-compile,%,$@)-host.. "
 	$(MAKE) -C $(patsubst %-host-compile,%,$@) hostpackage
 	$(CMD_TRACE) " done"
 	$(END_TRACE)
 
 %-compile:
-	$(START_TRACE) "package/$(patsubst %-compile,%,$@)-compile: "
+	$(START_TRACE) "package/$(patsubst %-compile,%,$@)-compile.. "
 	@if test -f $(ADK_TOPDIR)/.rebuild.$(patsubst %-compile,%,$@); then \
 		$(MAKE) -C $(patsubst %-compile,%,$@) clean ; \
 		rm $(ADK_TOPDIR)/.rebuild.$(patsubst %-compile,%,$@) ; \
@@ -72,13 +72,13 @@ endif
 	$(END_TRACE)
 
 %-install:
-	$(START_TRACE) "package/$(patsubst %-install,%,$@)-install: "
+	$(START_TRACE) "package/$(patsubst %-install,%,$@)-install.. "
 	$(MAKE) -C $(patsubst %-install,%,$@) install
 	$(CMD_TRACE) " done"
 	$(END_TRACE)
 
 %-clean:
-	$(START_TRACE) "package/$(patsubst %-clean,%,$@)-clean: "
+	$(START_TRACE) "package/$(patsubst %-clean,%,$@)-clean.. "
 	$(MAKE) -C $(patsubst %-clean,%,$@) clean
 	$(CMD_TRACE) " done"
 	$(END_TRACE)

package/busybox/Makefile
@@ -11,7 +11,7 @@ PKG_DESCR:=		core utilities for embedded systems
 PKG_SECTION:=		base/apps
 PKG_BUILDDEP:=		bzip2-host
 PKG_URL:=		http://www.busybox.net/
-PKG_SITES:=		http://www.busybox.net/downloads/
+PKG_SITES:=		https://www.busybox.net/downloads/
 
 DISTFILES:=		${PKG_NAME}-${PKG_VERSION}.tar.bz2
 

package/glib/Makefile
@@ -14,7 +14,7 @@ PKG_BUILDDEP:=		glib-host libffi gettext-tiny zlib libelf dbus
 HOST_BUILDDEP:=		libffi-host gettext-tiny-host python2-host
 PKG_NEEDS:=		threads
 PKG_URL:=		http://www.gtk.org/
-PKG_SITES:=		http://ftp.gnome.org/pub/gnome/sources/glib/$(PKG_EXTRAVER)/
+PKG_SITES:=		http://gensho.acc.umu.se/pub/gnome/sources/glib/$(PKG_EXTRAVER)/
 PKG_NOPARALLEL:=	1
 PKG_OPTS:=		dev noscripts
 
@@ -36,10 +36,10 @@ CONFIGURE_ENV+=		glib_cv_long_long_format=ll \
 			ac_cv_func_posix_getgrgid_r=yes
 
 HOST_STYLE:=		auto
-ifneq ($(OStype),CYGWIN)
+ifneq ($(OS_FOR_BUILD),CYGWIN)
 HOST_CFLAGS+=		-fPIC
 endif
-ifneq ($(OStype),Darwin)
+ifneq ($(OS_FOR_BUILD),Darwin)
 HOST_CONFIGURE_ARGS+=	--with-libiconv=no
 endif
 HOST_CONFIGURE_ARGS+=	--disable-fam \

package/openssl/Makefile
@@ -70,10 +70,10 @@ ifeq ($(ADK_TARGET_ARCH_X86_64),y)
 CONFIG:=		linux-x86_64
 endif
 
-ifneq (,$(filter CYGWIN%,${OStype}))
+ifneq (,$(filter CYGWIN%,${OS_FOR_BUILD}))
 HOSTCONFIG:=		Cygwin-x86_64
 endif
-ifeq ($(OStype),Darwin)
+ifeq ($(OS_FOR_BUILD),Darwin)
 HOSTCONFIG:=		darwin64-x86_64-cc
 endif
 

package/pkgconf/Makefile
@@ -9,6 +9,7 @@ PKG_RELEASE:=		1
 PKG_HASH:=		8f5f48e52ddef0c3cbffe93f32d5148041e0030b8798b49b0b5fe72f4968d7ec
 PKG_DESCR:=		smart pkg-config replacement
 PKG_SECTION:=		dev/tools
+HOST_BUILDDEP:=		sed-host
 PKG_URL:=		https://github.com/pkgconf/pkgconf
 PKG_SITES:=		http://rabbit.dereferenced.org/~nenolod/distfiles/
 
@@ -23,9 +24,9 @@ $(eval $(call PKG_template,PKGCONF,pkgconf,$(PKG_VERSION)-${PKG_RELEASE},${PKG_D
 hostpost-install:
 	$(CP) ./files/pkg-config $(STAGING_HOST_DIR)/usr/bin
 ifeq ($(ADK_TARGET_USE_STATIC_LIBS),y)
-	$(SED) "s#@@OPTS@@#--static#" $(STAGING_HOST_DIR)/usr/bin/pkg-config
+	PATH="$(HOST_PATH)" $(SED) "s#@@OPTS@@#--static#" $(STAGING_HOST_DIR)/usr/bin/pkg-config
 else
-	$(SED) "s#@@OPTS@@##" $(STAGING_HOST_DIR)/usr/bin/pkg-config
+	PATH="$(HOST_PATH)" $(SED) "s#@@OPTS@@##" $(STAGING_HOST_DIR)/usr/bin/pkg-config
 endif
 	(cd $(STAGING_HOST_DIR)/usr/bin && ln -sf pkg-config $(GNU_TARGET_NAME)-pkg-config)
 

package/squashfs/Makefile
@@ -8,7 +8,6 @@ PKG_VERSION:=		4.2
 PKG_RELEASE:=		1
 PKG_HASH:=		d9e0195aa922dbb665ed322b9aaa96e04a476ee650f39bbeadb0d00b24022e96
 PKG_DESCR:=		squashfs filesystem utilities
-HOST_BUILDDEP:=		xz-host
 PKG_SECTION:=		sys/fs
 PKG_SITES:=		${MASTER_SITE_SOURCEFORGE:=squashfs/}
 

commit caf92e5e74031e0461b490f9a06ca308723518a3

Fix for unavailable dev-help and pkg-help targets
Fixes: a94d74d9c875b ("rework prereq check")
Signed-off-by: Phil Sutter <phil@nwl.cc>

Makefile
@@ -4,5 +4,5 @@
 # GNU make and BSD make compatible make file wrapper
 MAKECMDGOALS+= ${.TARGETS}
 
-all v allmodconfig allnoconfig allyesconfig help targethelp kernelconfig image menuconfig defconfig oldconfig download clean cleankernel cleansystem cleandir distclean hostclean hostpackage fetch package extract patch dep menu newpackage host-update-patches update-patches:
+all v allmodconfig allnoconfig allyesconfig help pkg-help dev-help targethelp kernelconfig image menuconfig defconfig oldconfig download clean cleankernel cleansystem cleandir distclean hostclean hostpackage fetch package extract patch dep menu newpackage host-update-patches update-patches:
 	@./scripts/prereq.sh ${MAKECMDGOALS}