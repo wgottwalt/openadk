196b86da481d0877f81023bbaf172c9c92fadd7b
Waldemar Brodkorb <wbx@uclibc-ng.org>
2016-10-09 17:28:28 +0200

    avoid error message

--- mk/build.mk
+++ mk/build.mk
@@ -323,7 +323,7 @@ $(CONFIG)/mconf:
 	@$(MAKE) -C $(CONFIG)
 
 defconfig: .menu $(CONFIG)/conf
-	-@rm $(ADK_TOPDIR)/.defconfig
+	-@rm $(ADK_TOPDIR)/.defconfig 2>/dev/null
 	@if [ ! -z "$(ADK_NO_CHECKSUM)" ];then \
 		echo "ADK_DISABLE_CHECKSUM=y" >> $(ADK_TOPDIR)/.defconfig; \
 	fi
