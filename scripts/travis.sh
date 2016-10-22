e5f6618dea0522c679c4526b56849f740fc5e7fc
Waldemar Brodkorb <wbx@uclibc-ng.org>
2016-10-22 19:00:35 +0200

    travis: add optional endian param

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 arch=$1
 system=$2
-make ADK_TARGET_OS=Linux ADK_TARGET_ARCH=$arch ADK_TARGET_SYSTEM=$system ADK_APPLIANCE=new defconfig
+vars="ADK_TARGET_OS=Linux ADK_TARGET_ARCH=$arch ADK_TARGET_SYSTEM=$system ADK_APPLIANCE=new"
+if [ ! -z $3 ]; then
+ endian=$3
+ vars="$vars ADK_TARGET_ENDIAN=$endian"
+fi
+make $vars defconfig
 make
