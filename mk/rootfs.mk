c76d1960526d15cbcef6a61b4f7bed8eb5b95037
Waldemar Brodkorb <wbx@openadk.org>
2016-11-28 17:34:43 +0100

    cleanup kernel rootfs choice, it is not well tested to use non-ext4 rootfs, add a linux kernel submenu

--- mk/rootfs.mk
+++ mk/rootfs.mk
@@ -12,18 +12,18 @@ ifeq ($(ADK_TARGET_QEMU),y)
 MTDDEV:=	root=/dev/mtdblock0
 ifeq ($(ADK_TARGET_ROOTFS_ARCHIVE),y)
 ifeq ($(ADK_TARGET_QEMU_WITH_VIRTIO),y)
-ROOTFS:=	root=/dev/vda1
+BLOCKDEV:=	root=/dev/vda1
 else
-ROOTFS:=	root=/dev/sda1 rootwait
+BLOCKDEV:=	root=/dev/sda1
 endif
 endif
 ifeq ($(ADK_TARGET_SYSTEM_QEMU_ARM_VEXPRESS_A9),y)
-ROOTFS:=	root=/dev/mmcblk0p1
+BLOCKDEV:=	root=/dev/mmcblk0p1
 endif
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_MIKROTIK_RB532),y)
-ROOTFS:=	root=/dev/sda2
+BLOCKDEV:=	root=/dev/sda2
 MTDDEV:=	root=/dev/mtdblock1
 endif
 
@@ -32,34 +32,32 @@ MTDDEV:=	root=/dev/mtdblock7
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_ACMESYSTEMS_FOXG20),y)
-ROOTFS:=	root=/dev/mmcblk0p2 rootwait
+BLOCKDEV:=	root=/dev/mmcblk0p2
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_SHARP_ZAURUS),y)
-ROOTFS:=	root=/dev/sda1 rootwait
+BLOCKDEV:=	root=/dev/sda1
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_RASPBERRY_PI),y)
-ROOTFS:=	root=/dev/mmcblk0p2 rootwait
+BLOCKDEV:=	root=/dev/mmcblk0p2
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_SOLIDRUN_IMX6),y)
-ROOTFS:=	root=/dev/mmcblk0p1 rootwait
+BLOCKDEV:=	root=/dev/mmcblk0p1
 endif
 
 ifeq ($(ADK_TARGET_SYSTEM_LEMOTE_YEELONG),y)
-USB:=		rootwait root=/dev/sdb1
-else
-USB:=		rootdelay=2
+USBDEV:=	root=/dev/sdb1
 endif
 
-$(eval $(call rootfs_template,usb,USB,$(USB)))
-$(eval $(call rootfs_template,archive,ARCHIVE,$(ROOTFS)))
+$(eval $(call rootfs_template,usb,USB,$(USBDEV) rootwait))
+$(eval $(call rootfs_template,archive,ARCHIVE,$(BLOCKDEV) rootwait))
 $(eval $(call rootfs_template,initramfs,INITRAMFS,rootfstype=tmpfs))
 $(eval $(call rootfs_template,initramfspiggyback,INITRAMFSPIGGYBACK,rootfstype=tmpfs))
 $(eval $(call rootfs_template,initramfsarchive,INITRAMFSARCHIVE,rootfstype=tmpfs))
 $(eval $(call rootfs_template,squashfs,SQUASHFS,$(MTDDEV) rootfstype=squashfs))
-$(eval $(call rootfs_template,yaffs,YAFFS,$(MTDDEV) panic=3))
+$(eval $(call rootfs_template,yaffs,YAFFS,$(MTDDEV)))
 $(eval $(call rootfs_template,jffs2,JFFS2,$(MTDDEV) rootfstype=jffs2))
 $(eval $(call rootfs_template,nfsroot,NFSROOT,root=/dev/nfs ip=dhcp))
 $(eval $(call rootfs_template,encrypted,ENCRYPTED))
