d5193809c182eb1e9914e3609b8cbe47f61545c2
Waldemar Brodkorb <wbx@uclibc-ng.org>
2018-03-07 21:20:55 +0100

    travis: check waldux

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-arch=$1
-system=$2
-vars="ADK_TARGET_OS=Linux ADK_TARGET_ARCH=$arch ADK_TARGET_SYSTEM=$system ADK_APPLIANCE=new"
-if [ ! -z $3 ]; then
- endian=$3
+os=$1
+arch=$2
+system=$3
+vars="ADK_TARGET_OS=$os ADK_TARGET_ARCH=$arch ADK_TARGET_SYSTEM=$system ADK_APPLIANCE=new"
+if [ ! -z $4 ]; then
+ endian=$4
  vars="$vars ADK_TARGET_ENDIAN=$endian"
 fi
 make $vars defconfig
